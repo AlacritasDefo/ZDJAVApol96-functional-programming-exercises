a6c6cdca259b69fc4345341c4787ea0866a4d985
rDk107c <radoslaw.kowalik90@gmail.com>
2021-08-24 19:36:58 +0200

    Precicate

---
+++ src/main/java/Predicate/Main.java
@@ -0,0 +1,32 @@
+package Predicate;
+
+import pl.sda.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/*Stwórz predykat sprawdzający czy użytkownik urodził się po roku 2000 a następnie w pętli dla
+kilku użytkowników wywołaj to sprawdzenie w klauzuli if. Jeśli użytkownik urodził się po roku 2000
+to wyświetl jego dane w konsoli.*/
+public class Main {
+    public static void main(String[] args) {
+        Predicate<User> after2000 = user -> user.getRokurodzenia()>2000;
+
+        User user1 = new User(1, "Damian", "Szczepaniak", "3663633636", 2002 );
+        User user2 = new User(2, "Robert", "Lewandowski", "9484734849", 1985 );
+        User user3 = new User(3, "Piotr", "Polak", "82828938483", 2010 );
+
+        List<User> listAfter2000 = new ArrayList<>();
+
+        listAfter2000.add(user1);
+        listAfter2000.add(user2);
+        listAfter2000.add(user3);
+
+        for (User user : listAfter2000){
+            if (after2000.test(user)){
+                System.out.println(user);
+            }
+        }
+    }
+}
